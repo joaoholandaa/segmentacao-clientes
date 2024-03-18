c51d39809d3c3b63c4cc43cceea9b80061bdf5b4
Joao Holanda <joaocmdt505@gmail.com>
2024-03-18 18:14:38 -0300

    :tada: Primeiro commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Realizar uma segmentação de clientes, tendo como objetivo identificar grupos de clientes que são mais atrativos e geram um maior faturamento na plataforma.',
+    author='João Pedro Holanda',
+    license='',
+)
